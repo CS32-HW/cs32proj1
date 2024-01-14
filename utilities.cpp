e2bdd1edfc1306756c2ea8c9b01d0ecb94469fb3
Ethan Cheng <ethan@nijika.org>
2024-01-13 21:44:34 -0800

    divide classes into different files

--- utilities.cpp
+++ utilities.cpp
@@ -1,4 +1,16 @@
+#include <random>
+using namespace std;
 
+// Return a uniformly distributed random int from min to max, inclusive
+int randInt(int min, int max)
+{
+    if (max < min)
+        swap(max, min);
+    static random_device rdv;
+    static default_random_engine generator(rdv());
+    uniform_int_distribution<> distro(min, max);
+    return distro(generator);
+}
 
 ///////////////////////////////////////////////////////////////////////////
 //  clearScreen implementation
